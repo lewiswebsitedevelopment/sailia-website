e95437d8fc1083724094342b8d4a791b7d2f266c
George Lewis <georgectlewis@gmail.com>
2021-12-12 00:06:03 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,13 @@ export default function Home() {
   return (
     <div class="font-montserrat">
       <Head>
-        <title>Create Next App</title>
+        <title>Sailia</title>
         <link rel="icon" href="/favicon.ico" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Roboto&display=swap" rel="stylesheet"></link>
       </Head>
+      <Header />
       <main class="flex-1 overflow-y-auto max-w-7xl mx-auto">
-        <div>
-          <h1 class="m-auto">Under construction</h1>
-        </div>
+        <h1>Under construction</h1>
       </main>
     </div>
   )
