29b9e5b404ff16c00402ef77fb05ee54f3acc9c8
George Lewis <georgectlewis@gmail.com>
2021-12-20 15:17:57 +0000

    Home page

--- components/header.js
+++ components/header.js
@@ -24,47 +24,97 @@ function Header() {
                         </a>
                     </Link>
                     <nav class="hidden md:block ml-auto">
-                        <ul class="flex space-x-14 text-sm leading-6 font-semibold text-gray-700">
-                            <li class='has-tooltip'>
-                                    <a class="h-full  hover:text-slate-500 duration-300">
-                                        <span class='tooltip grid rounded-2xl shadow-lg p-1 duration-300 shadow-2xl bg-white grid-cols-3 p-8 gap-x-10 gap-y-8 -mt-[-1.5rem]'>
-                                            <div>
+                        <ul class="flex space-x-14 text-sm leading-6 font-semibold">
+                            <li class='cursor-pointer has-tooltip'>
+                                    <a class=" hover:text-slate-500">
+                                        <span class='w-max tooltip grid rounded-2xl shadow-lg -translate-x-1/2  bg-white grid-cols-3 p-8 gap-x-10 gap-y-8 -mt-[-1.5rem]'>
+                                            <div class='flex w-max h-max'>
+                                                <div class="h-9 w-9 m-auto relative aspect-square">
+                                                    <Image
+                                                        loader={myLoader}
+                                                        src="websiteAssets/header/Management.svg"
+                                                        alt="Wheel"
+                                                        layout='fill'
+                                                        objectFit="contain"
+                                                        placeholder="blur" with animated shimmer blurDataURL
+                                                    />
+                                                </div>
                                                 <Link href="/features/Management">
-                                                    <a class="h-full  hover:text-slate-500 duration-300">
-                                                        <p>Management</p>
-                                                        <p>Get in control</p>
+                                                    <a class="h-full pl-3 text-custom-900 hover:text-slate-500">
+                                                        <p >Management</p>
+                                                        <p class='font-light'>Get in control</p>
                                                     </a>
                                                 </Link>
                                             </div>
-                                            <div>
+                                            <div class='flex w-max h-max'>
+                                                <div class="h-9 w-9 m-auto relative aspect-square">
+                                                    <Image
+                                                        loader={myLoader}
+                                                        src="websiteAssets/header/Booking.svg"
+                                                        alt="Wheel"
+                                                        layout='fill'
+                                                        objectFit="contain"
+                                                        placeholder="blur" with animated shimmer blurDataURL
+                                                    />
+                                                </div>
                                                 <Link href="/features/Booking">
-                                                    <a class="h-full  hover:text-slate-500 duration-300">
-                                                        <p>Booking</p>
-                                                        <p>All in one place</p>
+                                                    <a class="h-full pl-3 text-custom-900 hover:text-slate-500">
+                                                        <p >Booking</p>
+                                                        <p class='font-light'>All in one place</p>
                                                     </a>
                                                 </Link>
                                             </div>
-                                            <div>
+                                            <div class='flex w-max h-max'>
+                                                <div class="h-9 w-9 m-auto relative aspect-square">
+                                                    <Image
+                                                        loader={myLoader}
+                                                        src="websiteAssets/header/globe.svg"
+                                                        alt="Wheel"
+                                                        layout='fill'
+                                                        objectFit="contain"
+                                                        placeholder="blur" with animated shimmer blurDataURL
+                                                    />
+                                                </div>
                                                 <Link href="/features/Booking">
-                                                    <a class="h-full  hover:text-slate-500 duration-300">
-                                                        <p>Online Presence</p>
-                                                        <p>Get discovered</p>
+                                                    <a class="h-full pl-3 text-custom-900 hover:text-slate-500">
+                                                        <p >Online Precence</p>
+                                                        <p class='font-light'>Get discovered</p>
                                                     </a>
                                                 </Link>
                                             </div>
-                                            <div>
+                                            <div class='flex w-max h-max'>
+                                                <div class="h-9 w-9 m-auto relative aspect-square">
+                                                    <Image
+                                                        loader={myLoader}
+                                                        src="websiteAssets/header/Shop.svg"
+                                                        alt="Wheel"
+                                                        layout='fill'
+                                                        objectFit="contain"
+                                                        placeholder="blur" with animated shimmer blurDataURL
+                                                    />
+                                                </div>
                                                 <Link href="/features/Booking">
-                                                    <a class="h-full  hover:text-slate-500 duration-300">
-                                                        <p>Shop</p>
-                                                        <p>Sell anything</p>
+                                                    <a class="h-full pl-3 text-custom-900 hover:text-slate-500">
+                                                        <p >Shop</p>
+                                                        <p class='font-light'>Sell anything</p>
                                                     </a>
                                                 </Link>
                                             </div>
-                                            <div>
+                                            <div class='flex w-max h-max'>
+                                                <div class="h-9 w-9 m-auto relative aspect-square">
+                                                    <Image
+                                                        loader={myLoader}
+                                                        src="websiteAssets/header/Compliance.svg"
+                                                        alt="Wheel"
+                                                        layout='fill'
+                                                        objectFit="contain"
+                                                        placeholder="blur" with animated shimmer blurDataURL
+                                                    />
+                                                </div>
                                                 <Link href="/features/Booking">
-                                                    <a class="h-full  hover:text-slate-500 duration-300">
-                                                        <p>Compliance</p>
-                                                        <p>Every time</p>
+                                                    <a class="h-full pl-3 text-custom-900 hover:text-slate-500">
+                                                        <p >Compliance</p>
+                                                        <p class='font-light'>Every time</p>
                                                     </a>
                                                 </Link>
                                             </div>
@@ -74,12 +124,12 @@ function Header() {
                             </li>
                             <li>
                                 <Link href="/pricing">
-                                    <a class="hover:text-slate-500 duration-300">Pricing</a>
+                                    <a class="hover:text-slate-500">Pricing</a>
                                 </Link>
                             </li>
                             <li>
                                 <Link href="/about">
-                                    <a class="hover:text-slate-500 duration-300">About</a>
+                                    <a class="hover:text-slate-500">About</a>
                                 </Link>
                             </li>
                             <div class="border-solid border border-gray-700"></div>
--- components/index/setSail.js
+++ components/index/setSail.js
@@ -2,19 +2,17 @@ import Button from '../small/button'
 
 function Sail() {
     return (
-    <div class="max-w-7xl mx-auto container  flex flex-col md:flex-row items-center mt-10 p-5">
-
-		<div class="flex flex-col w-full lg:w-3/5 justify-center items-start pt-1 pb-12 px-6">
-			<h2 class="leading-normal mb-3 text-5xl">
-                Ready to set sail?
-            </h2>
+    <div class="bg-custom-600">
+      <div class="py-24 max-w-7xl mx-auto container  flex flex-col md:flex-row items-center mt-10 p-5">
+        <div class="flex flex-col w-full lg:w-3/5 justify-center items-start pt-1 pb-12 px-6">
+          <h2 class="leading-normal mb-3 text-5xl">Stop Organising. Get Sailing</h2>
             <Button 
-				link='/pricing'
-				text='See plans'
-			/>
+              link='/pricing'
+              text='See plans'
+            />
         </div>
-
-	</div>
+      </div>
+	  </div>
 
     )
   }
