90455d973e4a5d3a7634b88b73a63a91824e0241
George Lewis <georgectlewis@gmail.com>
2021-12-26 12:18:38 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,14 +14,14 @@ function Home() {
         <link rel="icon" href="/favicon.ico" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Roboto&display=swap" rel="stylesheet"></link>
       </Head>
-      <Header />
+      {/* <Header /> */}
       <main class=" w-full flex-1 overflow-y-auto">
-        <Hero />
-        <USP />
-        <SVP/>
-        <ToPricing text='Stop Organising. Get Sailing'/>
+        {/* <Hero /> */}
+        {/* <USP /> */}
+        {/* <SVP/> */}
+        {/* <ToPricing text='Stop Organising. Get Sailing'/> */}
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   )
 }
