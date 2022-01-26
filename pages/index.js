53315a0da27488d4da74ff90bb003af460e89125
George Lewis <georgectlewis@gmail.com>
2022-01-26 22:20:37 +0000

    Final

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ function Home({ data }) {
   return (
     <div class="w-full font-montserrat text-text-900">
       {/* <Head> */}
-      <title>Sailia {data}</title>
+      <title>Sailia </title>
       <link rel="icon" href="/favicon.ico" />
       <link
         href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Roboto&display=swap"
