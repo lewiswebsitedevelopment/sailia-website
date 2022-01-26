ed7509d8d49f589864e78eb24f397b1e1487def5
George Lewis <georgectlewis@gmail.com>
2022-01-26 21:55:14 +0000

    m

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,5 @@
 import Head from 'next/head'
-import Header from '../components/header'
-import Footer from '../components/footer'
-import Hero from '../components/index/hero'
-import USP from '../components/index/usp'
-import SVP from '../components/index/svp'
-import ToPricing from '../components/small/directToPricing'
 
-// function Home() {
-//   return (
-//     <div class="w-full font-montserrat text-text-900">
-//       <Head>
-//         <title>Sailia</title>
-//         <link rel="icon" href="/favicon.ico" />
-//         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Roboto&display=swap" rel="stylesheet"></link>
-//         <script dangerouslySetInnerHTML={{ __html: `
-//           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-//           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-//           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-//           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-//           })(window,document,'script','dataLayer','GTM-WT27B76');
-//           `}}>
-          
-//         </script>
-
-//         </Head>
-//       <Header /><noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WT27B76"
-// height="0" width="0" style="display:none;visibility:hidden"></iframe>
-// `}}></noscript>
-//       <main class=" w-full flex-1 overflow-y-auto">
-        
-//         <Hero />
-//         <USP />
-//         <SVP/>
-//         <ToPricing text='Stop Organising. Get Sailing'/>
-//       </main>
-//       <Footer />
-//     </div>
-//   )
-// }
-
-
-// export default Home
 
 const Home = ({ }) => {
   return (
@@ -64,7 +23,7 @@ const Home = ({ }) => {
           }}
         ></script>
       </Head>
-      <Header />
+      {/* <Header /> */}
       <noscript
         dangerouslySetInnerHTML={{
           __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WT27B76"
@@ -73,12 +32,12 @@ const Home = ({ }) => {
         }}
       ></noscript>
       <main class=" w-full flex-1 overflow-y-auto">
-        <Hero />
+        {/* <Hero />
         <USP />
         <SVP />
-        <ToPricing text="Stop Organising. Get Sailing" />
+        <ToPricing text="Stop Organising. Get Sailing" /> */}
       </main>
-      <Footer />
+      {/* <Footer />      */}
     </div>
   );
 };
