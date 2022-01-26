06487a865bd12fc1c63084c1ba2de9f6d76ca196
George Lewis <georgectlewis@gmail.com>
2022-01-26 22:14:28 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,27 @@
 // import Head from 'next/head'
 
 
-const Home = ({ }) => {
+const Home = ({ buildTimestamp }) => {
   return (
     <div class="w-full font-montserrat text-text-900">
       {/* <Head> */}
-        <title>Sailia</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Roboto&display=swap"
-          rel="stylesheet"
-        ></link>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
+      <title>Sailia {buildTimestamp}</title>
+      <link rel="icon" href="/favicon.ico" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Roboto&display=swap"
+        rel="stylesheet"
+      ></link>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-WT27B76');
           `,
-          }}
-        ></script>
+        }}
+      ></script>
       {/* </Head> */}
       {/* <Header /> */}
       <noscript
