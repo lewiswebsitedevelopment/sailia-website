abc40252c5825d8f2a9553eaa6795b14ef6ed0d2
George Lewis <georgectlewis@gmail.com>
2022-12-17 17:56:42 +0000

    Updated pricing

--- pages/pricing.js
+++ pages/pricing.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '../components/header'
 import Footer from '../components/footer'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -20,21 +21,21 @@ export default function Home() {
             <p class="lg:w-2/3  leading-relaxed text-base ">Plans for every size</p>
 
             </div>
-            <div class='w-full bg-custom-600 p-3 rounded-md'>
+            {/* <div class='w-full bg-custom-600 p-3 rounded-md'>
                 Contact us for more information
-            </div>
-            <div class="flex flex-wrap -m-4 opacity-0">
-            <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
+            </div> */}
+            <div class="flex flex-wrap -m-4 ">
+            <div class="p-4 xl:w-1/3 md:w-1/2 w-full">
                 <div class="h-full p-6 rounded-lg border bg-white border-custom-900 flex flex-col relative overflow-hidden">
-                <h2 class="text-sm tracking-widest title-font mb-1 font-medium">Launch</h2>
+                <h2 class="text-sm tracking-widest title-font mb-1 font-medium">Partner</h2>
                 <h1 class="text-5xl text-gray-900 pb-4 mb-4 border-b border-gray-200 leading-none">Free</h1>
-                <p class='mb-4 text-slate-600'>For Small Centres starting out</p>
+                <p class='mb-4 text-slate-600'>Become a partner centre</p>
                 <p class="flex items-center text-gray-600 mb-2">
                     <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                         <path d="M20 6L9 17l-5-5"></path>
                     </svg>
-                    </span>50 Clients
+                    </span>Unlimited support
                 </p>
                 
                 <p class="flex items-center text-gray-600 mb-2">
@@ -42,38 +43,38 @@ export default function Home() {
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                         <path d="M20 6L9 17l-5-5"></path>
                     </svg>
-                    </span>2 Instructors
+                    </span>Weekly meetings
                 </p>
                 <p class="flex items-center text-gray-600 mb-6">
                     <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                         <path d="M20 6L9 17l-5-5"></path>
                     </svg>
-                    </span>1 Location
+                    </span>Custom features
                 </p>
-                <button class="flex items-center mt-auto text-white bg-gray-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">Coming Soon
+                <a  href="/contact" class="flex items-center mt-auto text-white bg-gray-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">Contact us
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                     </svg>
-                </button>
-                <p class="text-xs text-gray-500 mt-3">Sign up available soon</p>
+                </a>
+                <p class="text-xs text-gray-500 mt-3"></p>
                 </div>
             </div>
-            <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
+            <div class="p-4 xl:w-1/3 md:w-1/2 w-full">
                 <div class="h-full p-6 rounded-lg border bg-white border-custom-800 flex flex-col relative overflow-hidden">
-                <span class="bg-custom-800 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">POPULAR</span>
-                <h2 class="text-sm tracking-widest title-font mb-1 font-medium">Bosun</h2>
+                {/* <span class="bg-custom-800 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">POPULAR</span> */}
+                <h2 class="text-sm tracking-widest title-font mb-1 font-medium">Classic</h2>
                 <h1 class="text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
-                    <span>£150</span>
-                    <span class="text-lg ml-1 font-normal text-gray-500">/mo</span>
+                    <span>3%</span>
+                    <span class="text-lg ml-1 font-normal text-gray-500"></span>
                 </h1>
-                <p class='mb-4 text-slate-600'>For growing small to medium sized centres</p>
+                <p class='mb-4 text-slate-600'>Our classic plan</p>
                 <p class="flex items-center text-gray-600 mb-2">
                     <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                         <path d="M20 6L9 17l-5-5"></path>
                     </svg>
-                    </span>1000 Clients
+                    </span>All booking features
                 </p>
                 
                 <p class="flex items-center text-gray-600 mb-2">
@@ -81,37 +82,37 @@ export default function Home() {
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                         <path d="M20 6L9 17l-5-5"></path>
                     </svg>
-                    </span>10 Instructors
+                    </span>Continous support
                 </p>
                 <p class="flex items-center text-gray-600 mb-6">
                     <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                         <path d="M20 6L9 17l-5-5"></path>
                     </svg>
-                    </span>2 Locations
+                    </span>Admin dashboard
                 </p>
-                <button class="flex items-center mt-auto text-white bg-custom-800 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">Coming Soon
+                <a  href="/contact" class="flex items-center mt-auto text-white bg-custom-800 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">Contact us
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                     </svg>
-                </button>
-                <p class="text-xs text-gray-500 mt-3">Sign up available soon</p>
+                </a>
+                <p class="text-xs text-gray-500 mt-3"></p>
                 </div>
             </div>
-            <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
+            <div class="p-4 xl:w-1/3 md:w-1/2 md:m-auto w-full">
                 <div class="h-full p-6 rounded-lg border bg-white border-custom-900 flex flex-col relative overflow-hidden">
-                <h2 class="text-sm tracking-widest title-font mb-1 font-medium">Crew</h2>
+                <h2 class="text-sm tracking-widest title-font mb-1 font-medium">Enterprise</h2>
                 <h1 class="text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
-                    <span>£300</span>
-                    <span class="text-lg ml-1 font-normal text-gray-500">/mo</span>
+                    <span>2.5%</span>
+                    <span class="text-lg ml-1 font-normal text-gray-500"></span>
                 </h1>
-                <p class='mb-4 text-slate-600'>For Large Established Centres</p>
+                <p class='mb-4 text-slate-600'>For larger centres</p>
                 <p class="flex items-center text-gray-600 mb-2">
                     <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                         <path d="M20 6L9 17l-5-5"></path>
                     </svg>
-                    </span>5000 Clients
+                    </span>Classic +
                 </p>
                 
                 <p class="flex items-center text-gray-600 mb-2">
@@ -119,61 +120,24 @@ export default function Home() {
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                         <path d="M20 6L9 17l-5-5"></path>
                     </svg>
-                    </span>20 Instructors
+                    </span>Enterprise support
                 </p>
                 <p class="flex items-center text-gray-600 mb-6">
                     <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                         <path d="M20 6L9 17l-5-5"></path>
                     </svg>
-                    </span>4 Locations
+                    </span>Custom features
                 </p>
-                <button class="flex items-center mt-auto text-white bg-gray-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">Coming Soon
+                <a  href="/contact" class="flex items-center mt-auto text-white bg-gray-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">Contact us
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                     </svg>
-                </button>
-                <p class="text-xs text-gray-500 mt-3">Sign up available soon</p>
+                </a>
+                <p class="text-xs text-gray-500 mt-3"></p>
                 </div>
             </div>
-            <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
-                <div class="h-full p-6 rounded-lg border bg-white border-custom-900 flex flex-col relative overflow-hidden">
-                <h2 class="text-sm tracking-widest title-font mb-1 font-medium">Helm</h2>
-                <h1 class="text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
-                    <span>£450</span>
-                    <span class="text-lg ml-1 font-normal text-gray-500">/mo</span>
-                </h1>
-                <p class='mb-4 text-slate-600'>Complete control for any sized centre</p>
-                <p class="flex items-center text-gray-600 mb-2">
-                    <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
-                        <path d="M20 6L9 17l-5-5"></path>
-                    </svg>
-                    </span>Unlimited Clients
-                </p>
-                
-                <p class="flex items-center text-gray-600 mb-2">
-                    <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
-                        <path d="M20 6L9 17l-5-5"></path>
-                    </svg>
-                    </span>Unlimited Instructors
-                </p>
-                <p class="flex items-center text-gray-600 mb-6">
-                    <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
-                        <path d="M20 6L9 17l-5-5"></path>
-                    </svg>
-                    </span>Unlimited Locations
-                </p>
-                <button class="flex items-center mt-auto text-white bg-gray-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">Coming Soon
-                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
-                    <path d="M5 12h14M12 5l7 7-7 7"></path>
-                    </svg>
-                </button>
-                <p class="text-xs text-gray-500 mt-3">Sign up available soon</p>
-                </div>
-            </div>
+            
             </div>
         </div>
         </section>
