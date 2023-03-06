cff3d5e4ac0d3ba19f6319c502416d143f0d2835
George Lewis <georgectlewis@gmail.com>
2023-03-06 12:42:51 +0100

    Plusible analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
-
+import Script from 'next/script' 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Script src="https://plausible.io/js/script.js" />
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp
